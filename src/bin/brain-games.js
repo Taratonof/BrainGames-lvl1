465ab289091283c33143e055f2b46a02e2de1751
Artur <taratonofa@yandex.ru>
2019-06-13 18:44:51 +0300

    4 шаг готов

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import actual from '..';
+import { brainGame } from '..';
 
-console.log('Welcome to the Brain Games!');
-console.log(actual());
+brainGame();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import readlineSync from 'readline-sync';
 
-const actual = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return `Hello, ${name}!`;
+export const actual = str => readlineSync.question(str);
+
+export const even = number => number % 2 === 0;
+
+export const randomInt = () => parseInt(Math.random() * 100, 10);
+
+export const brainGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('');
+  console.log(`Hello, ${actual('May I have your name? ')}!`);
 };
 
-export default actual;
+const startEvenGame = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const number = randomInt();
+    console.log(`Question: ${number}`);
+    const data = actual('Your answer: ');
+
+    if (even(number)) {
+      if (data === 'yes') {
+        console.log('Correct!');
+      } else {
+        return `'${data}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, `;
+      }
+    }
+    if (!even(number)) {
+      if (data === 'no') {
+        console.log('Correct!');
+      } else {
+        return `'${data}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, `;
+      }
+    }
+  }
+  return 'Congratulations, ';
+};
+
+export const brainEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  console.log('');
+  const name = actual('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('');
+  console.log(`${startEvenGame()} ${name}!`);
+};
