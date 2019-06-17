bd7683adb20e408880d59e5b2322eb36e30a8ce3
Artur <taratonofa@yandex.ru>
2019-06-17 14:57:08 +0300

    подправил ошибки линтера

--- src/games/brainGcd.js
+++ src/games/brainGcd.js
@@ -22,8 +22,8 @@ export default () => {
   const nod1 = nod(number1, number2);
   const nod2 = nod(number3, number4);
   const nod3 = nod(number5, number6);
-  const pair1 = cons(number1 + ' ' + number2, nod1 + '');
-  const pair2 = cons(number3 + ' ' + number4, nod2 + '');
-  const pair3 = cons(number5 + ' ' + number6, nod3 + '');
+  const pair1 = cons(`${number1} ${number2}`, `${nod1}`);
+  const pair2 = cons(`${number3} ${number4}`, `${nod2}`);
+  const pair3 = cons(`${number5} ${number6}`, `${nod3}`);
   console.log(startGame(pair1, pair2, pair3, name));
 };
