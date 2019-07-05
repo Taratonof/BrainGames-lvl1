b67799250cbc3b49b4196e0cb480cd6d93171f6f
Artur <taratonofa@yandex.ru>
2019-07-05 21:06:25 +0300

    рефакторинг 6

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,10 +8,6 @@ const isPrime = (number) => {
   if (number < 2) {
     return false;
   }
-  if (number === 2) {
-    return true;
-  }
-
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
